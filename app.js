dec80b77aac43d054957bec8995e5116fc236322
Mike Lofgren <mikeylofgren@gmail.com>
2019-11-13 22:00:05 -0800

    utils

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
-var stuff = require("./stuff");
+var events = require("events");
+var util = require("util");
 
-console.log(stuff.counter(["shaun", "crystal", "Mike"]));
-console.log(stuff.adder(5, 6));
-console.log(stuff.adder(stuff.pi, 5));
+var Person = function(name) {
+  this.name = name;
+};
+
+util.inherits(Person, events.EventEmitter);
+
+var james = new Person("james");
+var mary = new Person("mary");
+var mike = new Person("mike");
+var people = [james, mike, mary];
+
+people.forEach(function(person) {
+  person.on("speak", function(msg) {
+    console.log(person.name + " said: " + msg);
+  });
+});
+
+mike.emit("speak", "hey dudes");
